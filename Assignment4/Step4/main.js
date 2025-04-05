18cf07475a67a0c5ad0bb5023fbe884e0d7eb3e8
KieranMorgan00 <kieran.morgan1@dcmail.ca>
2025-04-04 21:58:47 -0400

    Remodified

--- Assignment4/Step4/main.js
+++ Assignment4/Step4/main.js
@@ -8,7 +8,6 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 
-
 function random(min,max) {
   const num = Math.floor(Math.random()*(max-min)) + min;
   return num;
