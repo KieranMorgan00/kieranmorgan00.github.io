ed01b1afe232553a758c1b7a45adce26a8c10a72
KieranMorgan00 <kieran.morgan1@dcmail.ca>
2025-04-04 20:18:38 -0400

    Added alt image constant array, added img inside of the thumb bar

--- Assignment4/Step2/gallery-start/main.js
+++ Assignment4/Step2/gallery-start/main.js
@@ -6,10 +6,26 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
-
 /* Declaring the alternative text for each image file */
-
+const alts = {
+    'pic1.jpg' : 'A real close picture of an eyeball',
+    'pic2.jpg' : 'A swerving sandy rock',
+    'pic3.jpg' : 'Assorted purple and white flowers',
+    'pic4.jpg' : 'Hieroglyphics depicting egyptian figures',
+    'pic5.jpg' : 'A big gross moth chilling on a leaf'
+}
 /* Looping through images */
+
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
