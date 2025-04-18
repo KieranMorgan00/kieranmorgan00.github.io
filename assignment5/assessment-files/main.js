61d0f6beaeb36374c1b914a2482ecc6a64aff0f9
KieranMorgan00 <kieran.morgan1@dcmail.ca>
2025-04-18 13:43:14 -0400

    Updated javascript to implement the aria changes

--- assignment5/assessment-files/main.js
+++ assignment5/assessment-files/main.js
@@ -5,15 +5,11 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
-    showHideBtn.textContent = 'Hide comments';
-    commentWrapper.style.display = 'block';
-  } else {
-    showHideBtn.textContent = 'Show comments';
-    commentWrapper.style.display = 'none';
-  }
+showHideBtn.onclick = function () {
+  let isHidden = commentWrapper.style.display === 'none';
+  showHideBtn.textContent = isHidden ? 'Hide comments' : 'Show comments';
+  showHideBtn.setAttribute('aria-expanded', isHidden ? 'true' : 'false');
+  commentWrapper.style.display = isHidden ? 'block' : 'none';
 };
 
 // functionality for adding a new comment via the comments form
